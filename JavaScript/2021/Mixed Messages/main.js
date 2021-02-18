afe9b0ed78a3dc949ebd1594f93a5cbdf556a738
MadhavMenon <73570612+MadhavMenon1007@users.noreply.github.com>
2021-02-18 11:14:16 +0530

    Added basic framework

--- JavaScript/2021/Mixed Messages/main.js
+++ JavaScript/2021/Mixed Messages/main.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+console.log("Welcome to mixed messages! All you have to do is type in the word \"Generate\" and we will generate a random message for you. \nHave fun!");
+
+const generate_message=()=>{
+
+    const message={
+        firstPart: [],
+        secondPart:[],
+        thirdPart: []
+    }
+    let randomNumber=Math.floor(Math.random()*this.firstPart.length)
+    return `${message.firstPart} ${message.secondPart} ${message.thirdPart}`
+}
+
